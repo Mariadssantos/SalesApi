461d67d90a15a799832273d14810a65ebbd19906
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-06-20 19:15:42 -0300

    Create service validateUser

---
+++ Services/UsersServices.cs
@@ -0,0 +1,21 @@
+﻿using MarcusDesafio.Data;
+using MarcusDesafio.Models;
+
+namespace MarcusDesafio.Services
+{
+    public class UsersServices
+    {
+        private readonly dbContext _context;
+        public UsersServices(dbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> validateUser(User user)
+        {
+            var result = _context.User.Where(x => x.Username.Contains(user.Username));
+            if (result.Any())
+                return false;
+            return true;
+        }
+    }
+}

2cc80b0649ef45113de1984a5d2290ed91faa8d0
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-06-20 19:23:26 -0300

    create JwtAuth configs for token generate

---
+++ Helpers/JwtAuth.cs
@@ -0,0 +1,49 @@
+﻿using MarcusDesafio.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MarcusDesafio.Helpers
+{
+    public class JwtAuth
+    {
+            public static string GenerateToken(User user)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("%fc105ba75146fd0/80ce91be53d181d02");
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, user.Username.ToString())
+                   // new Claim(ClaimTypes.Role, RoleFactory(user.TypeUser))
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(5),
+
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return tokenHandler.WriteToken(token);
+            }
+
+           /* private static string RoleFactory(int roleNumber)
+            {
+                switch (roleNumber)
+                {
+                    case 0:
+                        return "Director";
+
+                    case 1:
+                        return "Manager";
+
+                    default:
+                        throw new Exception();
+                }
+            }*/
+        
+}
+}

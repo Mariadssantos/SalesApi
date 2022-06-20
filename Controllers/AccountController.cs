b912ed91980adfe54ccb93555c7e23bf0cc5fcaa
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-06-20 19:24:04 -0300

    Create controller of auth

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using MarcusDesafio.Data;
+using MarcusDesafio.Helpers;
+using MarcusDesafio.Models;
+using MarcusDesafio.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarcusDesafio.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public AccountController(dbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("/api/v1/auth")]
+        public async Task<IActionResult> Auth([FromBody] User user)
+        {
+            try
+            {
+                //uma boa prática seria usar DI (Injeção de dependência)
+                //mas não é o foco do artigo
+
+                var userExists = _context.User.Where(x => x.Username.Contains(user.Username)).FirstOrDefault();
+
+                if (userExists == null)
+                    return BadRequest(new { Message = "Username e/ou senha está(ão) inválido(s)." });
+
+                var matchPassword = _context.User.Where(x => x.Password.Contains(user.Password));
+
+                if (matchPassword == null)
+                {
+                    return BadRequest(new { Message = "Email e/ou senha está(ão) inválido(s)." });
+                }
+
+                var token = JwtAuth.GenerateToken(userExists);
+
+                return Ok(new
+                {
+                    Token = token,
+                    Usuario = userExists
+                });
+
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { Message = "Ocorreu algum erro interno na aplicação, por favor tente novamente." });
+            }
+        }
+    }
+}

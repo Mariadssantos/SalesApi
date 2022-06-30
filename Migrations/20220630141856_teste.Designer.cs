028e920928a4c62a3439145812d18fef499b4063
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-06-30 11:39:30 -0300

    create migration for associate userId

--- Migrations/20220630141856_teste.Designer.cs
+++ Migrations/20220630141856_teste.Designer.cs
@@ -4,6 +4,7 @@ using MarcusDesafio.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarcusDesafio.Migrations
 {
     [DbContext(typeof(dbContext))]
-    partial class dbContextModelSnapshot : ModelSnapshot
+    [Migration("20220630141856_teste")]
+    partial class teste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
